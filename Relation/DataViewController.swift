64621d64fc9036e5ac7f4ddb82a48c567f267b9f
江宇揚 <yuyang@kousakaiage-no-macbook.local>
2018-11-14 22:54:09 +0900

    Initial Commit

---
+++ Relation/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  Relation
+//
+//  Created by 江宇揚 on 2018/11/14.
+//  Copyright © 2018 Jiang Yuyang. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
